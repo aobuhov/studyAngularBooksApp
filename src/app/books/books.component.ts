a907f955dde6348a86b4226c48fd025924c57e54
Aleksei Obukhov <alexey.obuhov@gmail.com>
2023-06-28 00:48:45 +0300

    creating html of books and rendering

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit{
+  name: string = 'Clean code';
+  author: string = 'Bod';
+  src: string = 'favicon.ico';
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
